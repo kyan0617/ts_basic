0050da47655098f40853b0d4844a219198026807
Haruka Nishida <nishida1436@gmail.com>
2022-08-03 19:22:18 +0900

    fixed world.ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true,
+    env: {
+      browser: true,
+      es2021: true,
+      node: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended", // TypeScriptでチェックされる項目をLintから除外する設定
+      "prettier", // prettierのextendsは他のextendsより後に記述する
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    plugins: ["@typescript-eslint"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      "ecmaVersion": "latest",
+      "sourceType": "module",
+      "project": "./tsconfig.json" // TypeScriptのLint時に参照するconfigファイルを指定
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+    root: true, // 上位ディレクトリにある他のeslintrcを参照しないようにする
+    rules: {}
+  }
--- src/world.ts
+++ src/world.ts
@@ -1,13 +1,23 @@
 export default class World {
-  message: string;
+  // クラスで使うプロパティ
+  message: string
 
+  // コンストラクタ（初期化）
   constructor(message: string) {
     this.message = message
   }
 
-  public sayHello(elem: HTMLElement | null): void {
-    if(elem) {
-      elem.innerText = this.message
+  // 外部から呼び出せるメソッドを定義
+  public sayHello(elem: HTMLElement | null) {
+    if (elem) {
+      return (elem.innerText = this.message)
+    } else {
+      return
     }
+
+
+
+
+    
   }
 }
