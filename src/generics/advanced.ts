49c5fd8080a853f73d8f6efb1a62649c6742cddc
Haruka Nishida <nishida1436@gmail.com>
2022-08-24 12:50:26 +0900

    add revture code

---
+++ src/generics/advanced.ts
@@ -0,0 +1,29 @@
+export default function genericAdvancedSample() {
+  // map関数のシグネチャ
+  type Map<T, U> = (array: T[], fn: (item: T) => U) => U[]
+
+  const mapStringsToNumbers: Map<string, number> = (array, fn) => {
+    const result = []
+    for (let i = 0; i < array.length; i++) {
+      const item = array[i]
+      result[i] = fn(item)
+    }
+
+    return result
+  }
+
+  const numbers = mapStringsToNumbers(["123", "456", "789"], (item) => Number(item))
+  console.log("Generic Advanced sample 1:", numbers)
+
+  const mapNumbersToStrings: Map<number, string> = (array, fn) => {
+    const result = []
+    for (let i = 0; i < array.length; i++) {
+      const item = array[i]
+      result[i] = fn(item)
+    }
+
+    return result
+  }
+  const strings = mapNumbersToStrings(numbers, (item) => String(item))
+  console.log("Generic Advanced sample 2:", strings)
+}
--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,55 @@
 // world.sayHello(root)
 
 // 03.基本の型定義
-import {anySample, notExistSample, primitiveSample, unknownSample} from './basic'
+// import {anySample, notExistSample, primitiveSample, unknownSample} from './basic'
 
-anySample()
-notExistSample()
-primitiveSample()
-unknownSample()
+// anySample()
+// notExistSample()
+// primitiveSample()
+// unknownSample()
+
+// 04.関数の型定義
+// import {logMessage,logMessage2, logMessage3, logMessage4 } from './function/basic'
+// import {isUserSignedIn, isUserSignedIn2, sumProductsPrice} from './function/parameters'
+
+// logMessage("Hello TypeScript")
+// logMessage2("Hello TypeScript2")
+// logMessage3("Hello TypeScript3")
+// logMessage4("Hello TypeScript4")
+// alwaysThrowError("Hello TypeScript5")
+
+// isUserSignedIn( "ABC", "Torahack" )
+// isUserSignedIn( "DEF" )
+// isUserSignedIn2( "ABC" )
+// const sum =sumProductsPrice(100, 200, 300, 400, 500)
+// console.log('FUNCTION parameters sample 5:', sum)
+
+// 05.オブジェクトの型定義
+// import objectSample from './object/object'
+// import typeAliasSample from './object/alias'
+
+// objectSample()
+// typeAliasSample()
+
+// 06.配列とタプルの型定義
+// import arraySample from './array/array'
+// import tupleSample from './array/tuple'
+
+// arraySample()
+// tupleSample()
+
+// 07.ジェネリック型とポリモーフィズム
+// import genericsBasicSample from "./generics/basic";
+// import genericAdvancedSample from "./generics/advanced";
+
+// genericsBasicSample()
+// genericAdvancedSample()
+
+// 10.非同期処理
+// import callbackSample from "./asynchronous/callback";
+// import promiseSample from "./asynchronous/promise";
+import asyncAwaitSample from "./asynchronous/asyncAwait";
+
+// callbackSample()
+// promiseSample()
+asyncAwaitSample()
